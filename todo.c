36b1344bcb71a730ecaa6c8246fa20790cc24b34
Gombos <gombos@stud.ntnu.no>
2012-02-03 00:07:46 +0100

    removed some files

--- todo.c
+++ todo.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("This is working!\n");
+	printf("%d", argc);
+	int i;
+	/*
+	for(i = 0; i <= 2; i++){
+		printf("%s", argv[i]);
+	}*/
+	printf("%s", argv[2]);
 }
