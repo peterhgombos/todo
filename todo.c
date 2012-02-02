075380198d80cc4635591229957ae76cdf4b501f
Gombos <gombos@stud.ntnu.no>
2012-02-03 00:38:58 +0100

    implemented help flag with getopt_long, starting to do stuff

--- todo.c
+++ todo.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <getopt.h>
+
+void help();
 
 int main(int argc, char *argv[])
 {
-	printf("%d", argc);
-	int i;
-	/*
-	for(i = 0; i <= 2; i++){
-		printf("%s", argv[i]);
-	}*/
-	printf("%s", argv[2]);
+	int c;
+
+	while (1)
+	{
+		static struct option long_options[] =
+		{
+			{"help",	no_argument,	0,	'h'},
+			{0, 0, 0, 0}
+		};
+		int option_index = 0;
+		c = getopt_long(argc, argv, "h", long_options, &option_index);
+
+		if(c == -1)
+			break;
+
+		switch(c)
+		{
+			case 'h':
+			help();
+			break;
+		}
+	}
+}
+
+void help()
+{
+	printf("This is the help file, should bring lots of help\n");
 }
