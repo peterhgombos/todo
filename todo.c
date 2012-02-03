ea7b3ba904f8969fa35603061d5413e4c1482e72
Gombos <gombos@stud.ntnu.no>
2012-02-03 13:14:50 +0100

    added add-option. Not saving anything yet

--- todo.c
+++ todo.c
@@ -2,6 +2,7 @@
 #include <getopt.h>
 
 void help();
+void add_todo(char string[]);
 
 int main(int argc, char *argv[])
 {
@@ -11,11 +12,12 @@ int main(int argc, char *argv[])
 	{
 		static struct option long_options[] =
 		{
-			{"help",	no_argument,	0,	'h'},
+			{"help",	no_argument,		0,	'h'},
+			{"add",		no_argument,	0,	'a'},
 			{0, 0, 0, 0}
 		};
 		int option_index = 0;
-		c = getopt_long(argc, argv, "h", long_options, &option_index);
+		c = getopt_long(argc, argv, "ha:", long_options, &option_index);
 
 		if(c == -1)
 			break;
@@ -25,6 +27,10 @@ int main(int argc, char *argv[])
 			case 'h':
 			help();
 			break;
+
+			case 'a':
+			add_todo(optarg);
+			break;
 		}
 	}
 }
@@ -32,4 +38,9 @@ int main(int argc, char *argv[])
 void help()
 {
 	printf("This is the help file, should bring lots of help\n");
+}
+
+void add_todo(char string[])
+{
+	printf("%s\n", string);
 }
